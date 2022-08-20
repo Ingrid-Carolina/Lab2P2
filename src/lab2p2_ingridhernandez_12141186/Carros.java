ad63460625f3fe359ec4c02f873eac14446dece0
Ingrid-Carolina <ingrid_hernandez@unitec.edu>
2022-08-20 08:43:37 -0600

    Avance 5

---
+++ src/lab2p2_ingridhernandez_12141186/Carros.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2p2_ingridhernandez_12141186;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author incah
+ */
+public class Carros {
+    
+}
---
+++ src/lab2p2_ingridhernandez_12141186/Clientes.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2p2_ingridhernandez_12141186;
+
+/**
+ *
+ * @author incah
+ */
+public class Clientes {
+    private String nombre;
+    private int edad;
+    private String ID;
+    private double saldo;
+
+    public Clientes() {
+    }
+
+    public Clientes(String nombre, int edad, String ID, double saldo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.ID = ID;
+        this.saldo = saldo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    
+}
