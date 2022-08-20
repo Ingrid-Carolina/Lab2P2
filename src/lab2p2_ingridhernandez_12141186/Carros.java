c127f18ea730edbbb14825162814a81013e4e1ac
Ingrid-Carolina <ingrid_hernandez@unitec.edu>
2022-08-20 09:07:54 -0600

    avance 6

--- src/lab2p2_ingridhernandez_12141186/Carros.java
+++ src/lab2p2_ingridhernandez_12141186/Carros.java
@@ -12,5 +12,82 @@ import java.util.ArrayList;
  * @author incah
  */
 public class Carros {
+    private String marca;
+    private String modelo;
+    private int ano;
+    private String fecha;
+    private ArrayList<String>Estado;
+    private ArrayList<String>dueno;
+    private double costo;
+
+    public Carros() {
+    }
+
+    public Carros(String marca, String modelo, int ano, String fecha, ArrayList<String> Estado, ArrayList<String> dueno, double costo) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.fecha = fecha;
+        this.Estado = Estado;
+        this.dueno = dueno;
+        this.costo = costo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public ArrayList<String> getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(ArrayList<String> Estado) {
+        this.Estado = Estado;
+    }
+
+    public ArrayList<String> getDueno() {
+        return dueno;
+    }
+
+    public void setDueno(ArrayList<String> dueno) {
+        this.dueno = dueno;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+    
     
 }
--- src/lab2p2_ingridhernandez_12141186/Lab2P2_IngridHernandez_12141186.java
+++ src/lab2p2_ingridhernandez_12141186/Lab2P2_IngridHernandez_12141186.java
@@ -43,19 +43,76 @@ public class Lab2P2_IngridHernandez_12141186 {
                     System.out.println("2. Empleados");
                     System.out.println("3. Automoviles");
                     System.out.println("4. Regresar a inicio de sesion");
-                     System.out.println("===================================");
+                    System.out.println("===================================");
                     int opcion = lea.nextInt();
-                    switch (opcion){
-                case 1:{
-                    
-                }//fin del case 1
-                break;
-                case 2:{
-                    
+                    switch (opcion) {
+                        case 1: {
+                            boolean centinela2 = true;
+                            while (centinela2 == true) {
+                                System.out.println("========Menus de Clientes===========");
+                                System.out.println("1)Crear Clientes");
+                                System.out.println("2)Listar Clientes");
+                                System.out.println("3)Listar carros por cliente");
+                                System.out.println("4)Modificar Clientes");
+                                System.out.println("5)Eliminar Clientes");
+                                System.out.println("6)Regresar al menu principal");
+                                System.out.println("Ingrese la opcion:");
+                                System.out.println("====================================");
+                                int opcion2 = lea.nextInt();
+                                switch (opcion2) {
+                                    case 1:
+
+                                        break;
+                                    case 2:
+
+                                        break;
+                                    case 3:
+
+                                        break;
+                                    case 4:
+
+                                        break;
+                                    case 5:
+
+                                        break;
+                                    case 6:
+
+                                        centinela2 = false;
+                                        break;
+
+                                }
+
+                            }
+                        }//fin del case 1
+                        break;
+                        case 2: {
+                            boolean centinela3 = true;
+                            while (centinela3 == true) {
+                                System.out.println("========Menus de Empleados===========");
+                                System.out.println("1)Crear Empleados");
+                                System.out.println("2)Listar Empleados");
+                                System.out.println("3)Modificar Empleados");
+                                System.out.println("4)Eliminar Empleados");
+                                System.out.println("5)Estado de Trabajos de los Empleados");
+                                System.out.println("6)Regresar al menu principal");
+                                System.out.println("Ingrese la opcion:");
+                                System.out.println("====================================");
+                                int opc3 = lea.nextInt();
+                            }
                 }//fin del case 2
                 break;
                 case 3:{
-                    
+                    boolean centinale4 = true;
+                            while (centinale4 == true) {
+                                 System.out.println("=============Menus de Carros=============");
+                                System.out.println("1)Añadir Carros al sistema");
+                                System.out.println("2)Listar carro");
+                                System.out.println("3)Modificar su estado de reparacion");
+                                System.out.println("4)Regresar al menu principal");
+                                System.out.println("Ingrese la opcion:");
+                                System.out.println("==========================================");
+                                int opc4 = lea.nextInt();
+                            }
                 }//fin del case 3
                 break;
                 case 4:
