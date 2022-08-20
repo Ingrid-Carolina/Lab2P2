bd2741117fd2898ad2c1b59cc108c2b6926f9e5f
Ingrid-Carolina <ingrid_hernandez@unitec.edu>
2022-08-20 08:15:27 -0600

    Avance 1

--- src/lab2p2_ingridhernandez_12141186/Lab2P2_IngridHernandez_12141186.java
+++ src/lab2p2_ingridhernandez_12141186/Lab2P2_IngridHernandez_12141186.java
@@ -5,17 +5,28 @@
  */
 package lab2p2_ingridhernandez_12141186;
 
+import java.util.Scanner;
+
 /**
  *
  * @author incah
  */
 public class Lab2P2_IngridHernandez_12141186 {
-
+static Scanner lea = new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        boolean centinale = true;
+        while (centinale == true){
+            System.out.println("=====INICION DE LA SECCION========");
+            System.out.println("Ingrese el Usuario: ");
+            String usuario =lea.next();
+            System.out.println("Ingrese la Contraseña: ");
+            String Contra =lea.next();
+            
+        }
     }
     
 }
